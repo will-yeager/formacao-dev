43e6995998b1f86dc525ca1f2dd689a7eb5bbaf2
William Jonathan <willyeager01@gmail.com>
2021-07-26 01:19:08 -0300

    Curso: Mockito

---
+++ Java/IdeaProjects/mock/src/main/java/br/com/alura/leilao/service/GeradorDePagamento.java
@@ -0,0 +1,31 @@
+package br.com.alura.leilao.service;
+
+import java.time.Clock;
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.alura.leilao.dao.PagamentoDao;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Pagamento;
+
+@Service
+public class GeradorDePagamento {
+
+	private PagamentoDao pagamentos;
+	private Clock clock;
+
+	public GeradorDePagamento(PagamentoDao pagamentos, Clock clock) {
+		this.pagamentos = pagamentos;
+		this.clock = clock;
+	}
+
+	@Autowired
+	public void gerarPagamento(Lance lanceVencedor) {
+		LocalDate vencimento = LocalDate.now(clock).plusDays(1);
+		Pagamento pagamento = new Pagamento(lanceVencedor, vencimento);
+		this.pagamentos.salvar(pagamento);
+	}
+
+}
