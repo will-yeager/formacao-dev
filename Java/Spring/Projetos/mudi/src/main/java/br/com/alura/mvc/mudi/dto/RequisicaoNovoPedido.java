9186aa89b98c633c676fed380c039089e320266e
William Jonathan <willyeager01@gmail.com>
2021-08-03 13:41:29 -0300

    Curso: Spring MVC 1 E 2

---
+++ Java/Spring/Projetos/mudi/src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -0,0 +1,70 @@
+package br.com.alura.mvc.mudi.dto;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.StatusPedido;
+
+import javax.validation.constraints.NotBlank;
+
+public class RequisicaoNovoPedido {
+
+    @NotBlank//NotBlank.requisicaoNovoPedido.nomeProduto=campo deve ser preenchido=não pode estar em branco
+    private String nomeProduto;
+
+    @NotBlank
+    private String urlProduto;
+
+    @NotBlank
+    private String urlImagem;
+    private String descricao;
+    private StatusPedido statusPedido;
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getUrlProduto() {
+        return urlProduto;
+    }
+
+    public void setUrlProduto(String urlProduto) {
+        this.urlProduto = urlProduto;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public StatusPedido getStatusPedido() {
+        return statusPedido;
+    }
+
+    public void setStatusPedido(StatusPedido statusPedido) {
+        this.statusPedido = statusPedido;
+    }
+
+    public Pedido toPedido() {
+        Pedido pedido = new Pedido();
+        pedido.setDescricao(descricao);
+        pedido.setNomeProduto(nomeProduto);
+        pedido.setUrlImagem(urlImagem);
+        pedido.setUrlProduto(urlProduto);
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+        return pedido;
+    }
+}
---
+++ Java/Spring/Projetos/mudi/src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -0,0 +1,113 @@
+package br.com.alura.mvc.mudi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class Pedido {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nomeProduto;
+    private BigDecimal valorNegociado;
+    private LocalDate dataDaEntrega;
+    private String urlProduto;
+    private String urlImagem;
+    private String descricao;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pedido", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Oferta> ofertas;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Oferta> getOfertas() {
+        return ofertas;
+    }
+
+    public void setOfertas(List<Oferta> ofertas) {
+        this.ofertas = ofertas;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Enumerated(EnumType.STRING)
+    private StatusPedido status;
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public BigDecimal getValorNegociado() {
+        return valorNegociado;
+    }
+
+    public void setValorNegociado(BigDecimal valorNegociado) {
+        this.valorNegociado = valorNegociado;
+    }
+
+    public LocalDate getDataDaEntrega() {
+        return dataDaEntrega;
+    }
+
+    public void setDataDaEntrega(LocalDate dataDaEntrega) {
+        this.dataDaEntrega = dataDaEntrega;
+    }
+
+    public String getUrlProduto() {
+        return urlProduto;
+    }
+
+    public void setUrlProduto(String urlProduto) {
+        this.urlProduto = urlProduto;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public StatusPedido getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusPedido status) {
+        this.status = status;
+    }
+}
