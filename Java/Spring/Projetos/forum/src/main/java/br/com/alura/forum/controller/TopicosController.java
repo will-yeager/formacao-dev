0d684c3e75cf80670775dd8c25db2dd2e754501e
William Jonathan <willyeager01@gmail.com>
2021-08-16 23:54:25 -0300

    OWASP

--- Java/Spring/Projetos/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ Java/Spring/Projetos/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -9,6 +9,13 @@ import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -31,19 +38,26 @@ public class TopicosController {
     @Autowired
     private CursoRepository cursoRepository;
 
+
     @GetMapping
-    public List<TopicoDto> lista(String nomeCurso) {
+    @Cacheable(value = "listaDeTopicos")
+    public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,@PageableDefault(sort = "id", direction = Sort.Direction.DESC, page = 0, size = 10) Pageable paginacao) {
+                                 //@RequestParam int pagina,@RequestParam int qtd, @RequestParam String ordenacao) {
+
+        //Pageable paginacao = PageRequest.of(pagina, qtd, Sort.Direction.ASC, ordenacao);
+
         if(nomeCurso == null) {
-            List<Topico> topicos = topicoRepository.findAll();
+            Page<Topico> topicos = topicoRepository.findAll(paginacao);
             return TopicoDto.converter(topicos);
         } else {
-            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+            Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
             return TopicoDto.converter(topicos);
         }
     }
 
     @PostMapping
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 
         Topico topico = form.converter(cursoRepository);
@@ -65,6 +79,7 @@ public class TopicosController {
 
     @PutMapping("/{id}")
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
         Optional<Topico> optional = topicoRepository.findById(id);
         if(optional.isPresent()) {
@@ -77,6 +92,7 @@ public class TopicosController {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<?> remover(@PathVariable Long id) {
         Optional<Topico> optional = topicoRepository.findById(id);
         if(optional.isPresent()) {
---
+++ Java/Spring/Projetos/forum/src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -0,0 +1,47 @@
+package br.com.alura.forum.repository;
+
+import br.com.alura.forum.modelo.Curso;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class CursoRepositoryTest {
+
+    @Autowired
+    private CursoRepository repository;
+
+    @Autowired
+    private TestEntityManager em;
+
+    @Test
+    public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
+        String nomeCurso = "Spring Teste";
+
+        Curso springTeste = new Curso();
+        springTeste.setNome(nomeCurso);
+        springTeste.setCategoria("Programacao");
+        em.persist(springTeste);
+
+        Curso curso = repository.findByNome(nomeCurso);
+
+        Assert.assertNotNull(curso);
+        Assert.assertEquals(nomeCurso, curso.getNome());
+    }
+
+    @Test
+    public void naoDeveriaCarregarUmCursoCujoNomeNaoEsta() {
+        String nomeCurso = "TESTE TESTE";
+        Curso curso = repository.findByNome(nomeCurso);
+        Assert.assertNull(curso);
+    }
+}
